0f45fc034a80d5246e2cc72c9401ce980537e31b
Dwight Spencer <dspencer@computekindustries.com>
2017-12-02 11:47:38 -0600

    Update bundle.js

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,5 +1,21 @@
+// when DOM.ready:
 window.addEventListener('DOMContentLoaded', function() {
   document
     .querySelector('body')
     .bootstrapMaterialDesign();
 });
+
+// when Document.ready:
+window.addEventListener('load', function() {
+  /* global navigator */
+  if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator
+      .serviceWorker
+      .register('/scripts/serviceworker.js')
+      .then(function(reg)  { console.log("Yes, it did."); })
+      .catch(function(err) { console.log("No it didn't. This happened: ", err); });
+  }
+});
+
+// Everything else:
